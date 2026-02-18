42f8942ffc5e52c1358669402e00946667a8adaa
PupeTodorovic <millostodorovic.01@gmail.com>
2026-02-19 00:09:06 +0100

    Pronadji inode i prolaz kroz procese
    Added functionality to list all processes in the kernel log.

--- hello.c
+++ hello.c
@@ -3,31 +3,39 @@
 #include <linux/fs.h>
 #include <linux/path.h>
 #include <linux/dcache.h>
+#include <linux/sched/signal.h>
 
 static char *filepath = "/tmp/testfile.txt";
 module_param(filepath, charp, 0000);
-MODULE_PARM_DESC(filepath, "Putanja do fajla");
 
 static int __init hello_init(void)
 {
     struct path path;
     struct inode *inode;
+    struct task_struct *task;
     int ret;
 
     printk(KERN_INFO "Module loaded\n");
-    printk(KERN_INFO "Putanja: %s\n", filepath);
 
-    /* Pretvara putanju u kernel path */
+    /* Pronadji inode fajla */
     ret = kern_path(filepath, LOOKUP_FOLLOW, &path);
     if (ret) {
         printk(KERN_INFO "Ne mogu da nadjem fajl\n");
         return ret;
     }
 
-    /* Uzmi inode */
     inode = d_inode(path.dentry);
 
-    printk(KERN_INFO "Inode broj je: %lu\n", inode->i_ino);
+    printk(KERN_INFO "Inode broj: %lu\n", inode->i_ino);
+
+    printk(KERN_INFO "Lista svih procesa:\n");
+
+    /* Prolaz kroz sve procese */
+    for_each_process(task) {
+        printk(KERN_INFO "PID: %d | Ime: %s\n",
+               task->pid,
+               task->comm);
+    }
 
     path_put(&path);
 
