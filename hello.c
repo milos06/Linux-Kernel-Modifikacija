709079b96b4ab288e8c44c13d80253b692ad53d0
PupeTodorovic <millostodorovic.01@gmail.com>
2026-02-18 23:41:44 +0100

    Dodaj parametar putanje datoteke i ažuriraj poruke

--- hello.c
+++ hello.c
@@ -1,15 +1,21 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 
+static char *filepath = "/tmp/testfile.txt";
+
+module_param(filepath, charp, 0000);
+MODULE_PARM_DESC(filepath, "Putanja do fajla");
+
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "Hello kernel!\n");
+    printk(KERN_INFO "Module loaded\n");
+    printk(KERN_INFO "Fajl je: %s\n", filepath);
     return 0;
 }
 
 static void __exit hello_exit(void)
 {
-    printk(KERN_INFO "Goodbye kernel!\n");
+    printk(KERN_INFO "Module unloaded\n");
 }
 
 module_init(hello_init);
