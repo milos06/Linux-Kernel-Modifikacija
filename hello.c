6e5e6a1342536deb3f19f60f5aee2c96794cc903
PupeTodorovic <millostodorovic.01@gmail.com>
2026-02-19 00:11:06 +0100

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,43 +1,66 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/fs.h>
-#include <linux/path.h>
-#include <linux/dcache.h>
 #include <linux/sched/signal.h>
+#include <linux/fdtable.h>
+#include <linux/mm.h>
 
 static char *filepath = "/tmp/testfile.txt";
 module_param(filepath, charp, 0000);
 
 static int __init hello_init(void)
 {
-    struct path path;
-    struct inode *inode;
+    struct file *target_file;
+    struct inode *target_inode;
     struct task_struct *task;
-    int ret;
 
     printk(KERN_INFO "Module loaded\n");
 
-    /* Pronadji inode fajla */
-    ret = kern_path(filepath, LOOKUP_FOLLOW, &path);
-    if (ret) {
-        printk(KERN_INFO "Ne mogu da nadjem fajl\n");
-        return ret;
+    /* Otvori fajl u kernelu */
+    target_file = filp_open(filepath, O_RDONLY, 0);
+    if (IS_ERR(target_file)) {
+        printk(KERN_INFO "Ne mogu da otvorim fajl\n");
+        return PTR_ERR(target_file);
     }
 
-    inode = d_inode(path.dentry);
+    target_inode = file_inode(target_file);
 
-    printk(KERN_INFO "Inode broj: %lu\n", inode->i_ino);
-
-    printk(KERN_INFO "Lista svih procesa:\n");
+    printk(KERN_INFO "Trazim procese koji koriste inode: %lu\n",
+           target_inode->i_ino);
 
     /* Prolaz kroz sve procese */
     for_each_process(task) {
-        printk(KERN_INFO "PID: %d | Ime: %s\n",
-               task->pid,
-               task->comm);
+
+        struct files_struct *files = task->files;
+        struct fdtable *fdt;
+        unsigned int i;
+
+        if (!files)
+            continue;
+
+        spin_lock(&files->file_lock);
+        fdt = files_fdtable(files);
+
+        for (i = 0; i < fdt->max_fds; i++) {
+
+            struct file *file = fdt->fd[i];
+
+            if (file && file_inode(file) == target_inode) {
+
+                printk(KERN_INFO
+                       "PID: %d | Name: %s | Prio: %d | Nice: %d | Mem: %lu KB\n",
+                       task->pid,
+                       task->comm,
+                       task->prio,
+                       task_nice(task),
+                       task->mm ? (task->mm->total_vm << (PAGE_SHIFT - 10)) : 0);
+            }
+        }
+
+        spin_unlock(&files->file_lock);
     }
 
-    path_put(&path);
+    filp_close(target_file, NULL);
 
     return 0;
 }
