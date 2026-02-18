f2fd96c5b45c9d233dc082d99c22738c4b15f45a
PupeTodorovic <millostodorovic.01@gmail.com>
2026-02-18 23:52:16 +0100

    Update

--- hello.c
+++ hello.c
@@ -1,15 +1,36 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/path.h>
+#include <linux/dcache.h>
 
 static char *filepath = "/tmp/testfile.txt";
-
 module_param(filepath, charp, 0000);
 MODULE_PARM_DESC(filepath, "Putanja do fajla");
 
 static int __init hello_init(void)
 {
+    struct path path;
+    struct inode *inode;
+    int ret;
+
     printk(KERN_INFO "Module loaded\n");
-    printk(KERN_INFO "Fajl je: %s\n", filepath);
+    printk(KERN_INFO "Putanja: %s\n", filepath);
+
+    /* Pretvara putanju u kernel path */
+    ret = kern_path(filepath, LOOKUP_FOLLOW, &path);
+    if (ret) {
+        printk(KERN_INFO "Ne mogu da nadjem fajl\n");
+        return ret;
+    }
+
+    /* Uzmi inode */
+    inode = d_inode(path.dentry);
+
+    printk(KERN_INFO "Inode broj je: %lu\n", inode->i_ino);
+
+    path_put(&path);
+
     return 0;
 }
 
